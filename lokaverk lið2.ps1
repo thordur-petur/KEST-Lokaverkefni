cbc99c9e3f9be7fb6864ff8c4d1d4392c94c784c
thordur petur <116312220+thordur-petur@users.noreply.github.com>
2023-04-27 12:17:21 +0000

    Add files via upload

---
+++ lokaverk lið2.ps1
@@ -0,0 +1,13 @@
+﻿
+$gogn = Import-Csv C:\Users\ThordurPetur\Desktop\hópar.csv
+foreach($faersla in $gogn){
+$name=$faersla.notendanafn
+$first=$faersla.nafn
+$last=$faersla.eftirnafn
+$fullname=$first + " " + $last
+$pass=ConvertTo-SecureString 'Lykkja08' -AsPlainText -Force
+$hopp=$faersla.hopur
+New-LocalUser -Name $name -Password $pass -FullName $fullname -Description "this is a test user"
+Add-LocalGroupMember -Group Allir -Member $name
+Add-LocalGroupMember -Group $hopp -Member $name
+}
